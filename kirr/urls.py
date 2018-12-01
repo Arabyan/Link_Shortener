63b5223e3b944cc49efe3d8576dc6bb80be86a72
Arabyan <grigorarabyan@yahoo.com>
2018-12-02 01:48:35 +0400

    added hosts and Html file to project

---
+++ kirr/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import url
+from django.contrib import admin
+
+
+from shortener.views import  HomeView, KirrCBView
+
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'^$', HomeView.as_view()),
+    url(r'^(?P<shortcode>[\w-]+){6,15}/$',KirrCBView.as_view()),
+]
---
+++ shortener/views.py
@@ -0,0 +1,18 @@
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
+from django.views import View
+# Create your views here.
+from .models import KirrURL
+
+
+class HomeView(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, "shortener/home.html", {})
+
+
+
+class KirrCBView(View):
+    def get(self, request,shortcode=None, *args, **kwargs):
+        obj = get_object_or_404(KirrURL, shortcode=shortcode)
+        return HttpResponseRedirect(obj.url)
+
